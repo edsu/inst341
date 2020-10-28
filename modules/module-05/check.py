f6b0f89a2f0d1148aa567421b50c4677064e2b18
Ed Summers <ehs@pobox.com>
2020-10-28 11:24:24 -0400

    check!

---
+++ modules/module-05/check.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import hashlib
+
+from pathlib import Path
+
+user = sys.argv[1]
+submissions = Path('submissions')
+manifest_file = submissions / user / 'manifest.json'
+manifest = json.load(manifest_file.open())
+
+def get_sha256(p):
+    data = p.open('rb').read()
+    h = hashlib.sha256(data)
+    return h.hexdigest()
+
+for entry in manifest:
+    p = submissions / entry['path']
+    sha256 = get_sha256(p)
+    if entry['sha256'] != get_sha256(p):
+        print(p)
